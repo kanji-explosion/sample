5c26ecb8ab2dc959f30318cd73a06bfac0e61059
kanji-explosion <38119995+kanji-explosion@users.noreply.github.com>
2020-04-29 11:21:37 +0900

    Init

---
+++ sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sample
+//
+//  Created by Mamoru Sonoda on 2020/04/29.
+//  Copyright © 2020 mamoru.sonoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
